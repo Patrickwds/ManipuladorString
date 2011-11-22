0ba7c13e2351dc0e18a01a96e9ee032514e0cf47
Patrick Menezes <patrickwds1@gmail.com>
2011-11-22 19:12:24 -0430

    Update src/ManipuladorDeString.java

--- src/ManipuladorDeString.java
+++ src/ManipuladorDeString.java
@@ -6,48 +6,53 @@ public class ManipuladorDeString {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("Digite a frase desejada: ");
-		
-		String fraseDeEntrada = entrada.nextLine();
-		
-		String ordenado = ordenar(fraseDeEntrada);
-		System.out.println(ordenado);
-		
+			System.out.println("Digite a frase que deseja ordenar:");
+			String fraseDeEntrada = entrada.nextLine();
+			
+			String ordenado = ordenar(fraseDeEntrada);
+			System.out.println("Sua frase ordenada ficou assim: "+ordenado);
+			continuar();		
 	}
 	
 	public static String ordenar(String frase) {
 
 		String[] palavras = frase.split(" ");
-		
-		int quantidadeDePalavras = palavras.length;
-		
-		for(int i = 0; i < quantidadeDePalavras - 1; i++) {	
-		String palavraAtual = palavras[i];
-		
-		String palavraAtualDois = palavras[i+1];
-		
-			boolean estaOrdenado = true;
-			
-			for(int j = 0; j < quantidadeDePalavras - 1 - i; j++) {
+					
+		for(int i = 0; i < palavras.length; i++){	
+			for(int j = 0; j < palavras.length-1; j++) {
 				
-				if(palavraAtual.length() > palavraAtualDois.length()) {
-					String aux = palavras[j];
-					palavras[j] = palavras[j + 1];
-					palavras[j + 1] = aux;
-					estaOrdenado = false;
-				}
+				String palavraAtual = palavras[j];
+				String palavraPosterior = palavras[j+1];
 				
+				if(palavraAtual.length() > palavraPosterior.length()) {
+					String variavelAux = palavras[j+1];
+					palavras[j+1] = palavras[j];
+					palavras[j] = variavelAux;
+				}	
 			}
-			if(estaOrdenado)
-				break;
-		}
-		
-		String resultado = "";
+		}	
+		String resultado ="";
 		
 		for(int k = 0; k < palavras.length; k++) {
-			resultado += palavras[k] + " ";
+			resultado += " "+palavras[k];
 		}
+		return resultado.trim();
+	}
+	private static void continuar(){
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Ordenar outra frase?:");
+		System.out.println("Digite \"1\" para SIM:");
+		System.out.println("Digite \"2\" para NÃO:");
+		int continua = entrada.nextInt(); 
 		
-		return resultado;
+		switch (continua) {
+		case 1:
+			main(null);
+			break;
+		case 2:
+			System.out.print("\\-----Programa finalizado------\\");
+			break;
+		}
 	}
 }
